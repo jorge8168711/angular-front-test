ce0059de799a5f82e472922491899cfb0db6b2c4
Jorge Barron <jorgeb8168711@gmail.com>
2020-09-20 19:43:27 -0500

    Fix tailwind build

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,17 @@ module.exports = {
         test: /\.scss$/,
         loader: 'postcss-loader',
         options: {
-          ident: 'postcss',
-          syntax: 'postcss-scss',
-          plugins: () => [
-            require('postcss-import'),
-            require('tailwindcss'),
-            require('autoprefixer'),
-          ],
+          postcssOptions: {
+            ident: 'postcss',
+            syntax: 'postcss-scss',
+            plugins: [
+              ['postcss-import'],
+              ['tailwindcss'],
+              ['autoprefixer']
+            ],
+          },
         },
       },
     ],
   },
-};
+}
